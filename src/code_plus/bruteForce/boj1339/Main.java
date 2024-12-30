b3c3b9d25b3bc3835ec211c54763eae6a8ec3a02
ukly <dlwjddnr5438@kookmin.ac.kr>
2024-12-30 15:53:29 +0900

    code.plus-521

---
+++ src/code_plus/bruteForce/boj1339/Main.java
@@ -0,0 +1,36 @@
+package code_plus.bruteForce.boj1339;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.*;
+
+public class Main {
+    private static int n, answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        List<String> words = new ArrayList<>();
+        int[] alphabets = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            String word = br.readLine();
+            words.add(word);
+        }
+
+        for(String word : words){
+            for(int i=0; i<word.length(); i++){
+                char c = word.charAt(word.length() - i - 1);
+                alphabets[c-65] += (int)Math.pow(10, i);
+            }
+        }
+        Arrays.sort(alphabets);
+        for(int i=9; i>-1; i--){
+            answer += alphabets[16+i] * i;
+        }
+
+        System.out.println(answer);
+    }
+}
---
+++ src/code_plus/bruteForce/boj2529/Main.java
@@ -0,0 +1,65 @@
+package code_plus.bruteForce.boj2529;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private static int n;
+    private static String input;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        input = br.readLine();
+        List<String> list = new ArrayList<>(Arrays.asList(input.split(" ")));
+
+        Stack<Integer> maxStack = new Stack<>();
+        Stack<Integer> minStack = new Stack<>();
+
+        String answerMax = "";
+        String answerMin = "";
+
+        maxStack.add(9);
+        minStack.add(0);
+
+        for (int i = 1; i <= n; i++) {
+            String equalSign = list.get(i-1);
+            if(equalSign.equals(">")){
+                //max 값 찾기
+                while(!maxStack.isEmpty()){
+                    int num = maxStack.pop();
+                    answerMax += num;
+                }
+                maxStack.add(9-i);
+
+                //min 값 찾기
+                minStack.add(0+i);
+            }
+            else{
+                //max 값 찾기
+                maxStack.add(9-i);
+
+                //min 값 찾기
+                while(!minStack.isEmpty()){
+                    int num = minStack.pop();
+                    answerMin += num;
+                }
+                minStack.add(0+i);
+            }
+        }
+        while(!maxStack.isEmpty()){
+            int num = maxStack.pop();
+            answerMax += num;
+        }
+        while(!minStack.isEmpty()){
+            int num = minStack.pop();
+            answerMin += num;
+        }
+
+        System.out.println(answerMax);
+        System.out.println(answerMin);
+    }
+}
